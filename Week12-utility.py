c288b90ccac2596792dba1e641a498bc65b8ef9b
Mitchell Long <mitchelllong@mymail.mines.edu>
2019-11-19 10:03:57 -0700

    LoadFile function

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,7 +1,14 @@
-# PrintOutput function building
+# LoadFunction function building
 # Mitchell Long
 # CSCI 102 -- Section B
 # Week 12 - Part A
 
 def PrintOutput(a):
     print("OUTPUT", a)
+
+def LoadFile(filename):
+    f = open(filename)
+    b = f.readlines()
+    f.close()
+    return b
+
